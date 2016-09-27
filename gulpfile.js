87e227fc452a65107b3820c513fbc4b963a07467
Elf Sundae <elf.sundae@gmail.com>
2016-09-28 02:47:31 +0800

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/*!
+ * This is an example for elixir usage.
+ */
+
+const elixir = require('laravel-elixir');
+
+elixir.config.publicPath = 'build';
+elixir.config.css.minifier.pluginOptions = {
+  compatibility: 'ie7',
+  keepSpecialComments: 0
+};
+
+elixir(mix => {
+
+  // Bootstrap
+  mix.less('./bootstrap/bootstrap.less');
+  // mix.sass('./bootstrap-sass/_bootstrap.scss');
+
+  // AdminLTE
+  mix.less('./admin-lte/AdminLTE.less');
+
+});
