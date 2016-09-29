0664bf2f480721075f276f44ab254a5d2d971792
Elf Sundae <elf.sundae@gmail.com>
2016-09-30 00:05:24 +0800

    minor fix

--- utilities/csrf_token.js
+++ utilities/csrf_token.js
@@ -1,16 +1,14 @@
-/**
- * Add "X-CSRF-TOKEN" request header for Ajax, with value of meta named "csrf_token".
- *
- * @see https://laravel.com/docs/5.3/csrf#csrf-x-csrf-token
- */
 ;
-(function() {
-  if (typeof jQuery !== "undefined") {
-    $.ajaxPrefilter(function(options, originalOptions, xhr) {
-      var csrf_token = $('meta[name="csrf-token"]').attr('content');
-      if (csrf_token) {
-        xhr.setRequestHeader('X-CSRF-TOKEN', csrf_token);
-      }
-    });
-  }
-}());
+(function($) {
+  /**
+   * Add "X-CSRF-TOKEN" request header for Ajax, with value of meta named "csrf_token".
+   *
+   * @see https://laravel.com/docs/5.3/csrf#csrf-x-csrf-token
+   */
+  $.ajaxPrefilter(function(options, originalOptions, xhr) {
+    var csrf_token = $('meta[name="csrf-token"]').attr('content');
+    if (csrf_token) {
+      xhr.setRequestHeader('X-CSRF-TOKEN', csrf_token);
+    }
+  });
+})(jQuery);
