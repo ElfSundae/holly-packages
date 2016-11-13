50922426ab0871b8ce509bb8b4473a674188547e
Elf Sundae <elf.sundae@gmail.com>
2016-11-13 11:26:27 +0800

    Add code comment

--- datatables.net/datatables-helper.js
+++ datatables.net/datatables-helper.js
@@ -4,6 +4,9 @@
 
   /**
    * Find the closest "tr" for this element.
+   *
+   * If the Responsive extension is enabled, `$.fn.closest('tr')` may return
+   * the child view when there's one, this function fixes this issue.
    */
   $.fn.dataTableRow = function () {
     var $row = $(this).closest('tr');
