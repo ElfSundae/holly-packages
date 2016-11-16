04afc4c2c4ffb96a33b02e4fe46cf912f9ee225d
Elf Sundae <elf.sundae@gmail.com>
2016-11-17 05:49:18 +0800

    Change bootnotifyApi default message

--- bootnotify/bootnotifyApi.js
+++ bootnotify/bootnotifyApi.js
@@ -14,15 +14,16 @@
       options = {};
     }
 
-    var type = 'success';
+    var type = 'info';
     if (json === undefined || json.code === undefined) {
       type = 'danger';
-      options.message = 'bootnotifyApi exception: invalid JSON.';
+      options.message = 'bootnotifyApi exception: invalid api response.';
     } else if (1 == json.code) {
-      options.message = (json.msg !== undefined ? json.msg : 'Succeed!');
+      type = 'success';
+      options.message = (json.msg !== undefined ? json.msg : '操作成功！');
     } else {
       type = 'danger';
-      var msg = (json.msg !== undefined ? json.msg : 'Failed!');
+      var msg = (json.msg !== undefined ? json.msg : '操作失败！');
       if (msg.indexOf('\n') > -1) {
         msg = msg.replace(/\n/g, '<br>');
       }
