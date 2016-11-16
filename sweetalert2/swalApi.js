f14b1bec9ef9a4449315e9600f85f703dc9a693a
Elf Sundae <elf.sundae@gmail.com>
2016-11-17 06:07:17 +0800

    Add swalApi function

--- sweetalert2/swalApi.js
+++ sweetalert2/swalApi.js
@@ -2,5 +2,30 @@
 (function(swal) {
   "use strict";
 
+  window.swalApi = function (json, options) {
+    if (typeof(options) != 'object') {
+      options = {};
+    }
 
-})(swal);
+    var type = null;
+    if (json === undefined || json.code === undefined) {
+      options.html = 'swalApi exception: invalid api response.';
+    } else if (1 == json.code) {
+      type = 'success'
+      options.html = (json.msg !== undefined ? json.msg : '操作成功！');
+    } else {
+      type = 'error';
+      var msg = (json.msg !== undefined ? json.msg : '操作失败！');
+      if (msg.indexOf('\n') > -1) {
+        msg = msg.replace(/\n/g, '<br>');
+      }
+      options.html = msg;
+    }
+
+    if (typeof(options.type) === "undefined") {
+      options.type = type;
+    }
+
+    return swal(options);
+  };
+})(window, Sweetalert2);
