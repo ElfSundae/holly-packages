30146238b39ed0f8b526b720e94212145ba0a755
Elf Sundae <elf.sundae@gmail.com>
2017-11-29 18:36:29 +0800

    order

--- datatables/datatables-defaults.js
+++ datatables/datatables-defaults.js
@@ -2,11 +2,11 @@
   "use strict";
 
   $.extend($.fn.dataTable.defaults, {
+    "responsive": true,
+    "fixedHeader": true,
     "pagingType": "full_numbers",
     "lengthMenu": [15, 30, 50, 100],
     "pageLength": 15,
-    "responsive": true,
-    "fixedHeader": true,
     "language": {
       "emptyTable": "<span style='color:red'>无数据</span>",
       "info": "当前 _START_-_END_ 项，共 _TOTAL_ 项",
